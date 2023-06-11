ccd7d252296d7ada0d1d178e78a8a94f990a5632
Kamil Ciągło <kamilciag123@gmail.com>
2023-06-11 13:03:58 +0200

    Font works on WebGL
    finally...

--- Assets/Scripts/Narrator.cs
+++ Assets/Scripts/Narrator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class Narrator : MonoBehaviour
 {
     public AudioClip audio;
     public string audioText;
-    public Text textObject;
+    public TextMeshProUGUI textObject;
     public GameObject canvas;
     public GameObject nextAudio;
     
